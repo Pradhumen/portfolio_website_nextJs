4e07b5b2f0f7700acdede5694f1ef03ef0f8219a
pradhumen <pradhumensingh1112@gmail.com>
2024-07-07 20:58:22 +0530

    add stair animation on page change

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Variable, Weight } from "lucide-react";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"], Weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetbrainsMono.Variable}>
         <Header />
+        <StairTransition/>
         <PageTransition>
           {children}
         </PageTransition>
